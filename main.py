591d7b8f417e54e780b24aae9174ea87d8dc1fff
v1n1sk0 <maksimserbakov484@gmail.com>
2025-06-18 11:51:33 +0300

    Zaдaчa 3

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class AbstractProduct(ABC):
         pass
 
 
+from abc import ABC, abstractmethod
+
+class AbstractProduct(ABC):
+    @abstractmethod
+    def get_description(self) -> str:
+        pass
+
+
 class Product(AbstractProduct):
     def __init__(self, name: str, quantity: int, price: float):
         if quantity < 0:
@@ -15,7 +23,15 @@ class Product(AbstractProduct):
             raise ValueError("Цена не может быть меньше нуля!")
         self.name = name
         self.quantity = quantity
-        self.price = price
+        self.__price = price
+
+    def get_price(self) -> float:
+        return self.__price
+
+    def set_price(self, new_price: float):
+        if new_price < 0:
+            raise ValueError("Цена не может быть меньше нуля!")
+        self.__price = new_price
 
     def __add__(self, other):
         if isinstance(other, Product):
@@ -23,22 +39,22 @@ class Product(AbstractProduct):
             if total_quantity == 0:
                 avg_price = 0
             else:
-                avg_price = ((self.price * self.quantity) + (other.price * other.quantity)) / total_quantity
+                avg_price = ((self.get_price() * self.quantity) + (other.get_price() * other.quantity)) / total_quantity
             return Product(f"{self.name} + {other.name}", total_quantity, avg_price)
         raise TypeError("Можно складывать только объекты Product")
 
     def __lt__(self, other):
         if isinstance(other, Product):
-            return self.price < other.price
+            return self.get_price() < other.get_price()
         raise TypeError("Можно сравнивать только объекты Product")
 
     def __gt__(self, other):
         if isinstance(other, Product):
-            return self.price > other.price
+            return self.get_price() > other.get_price()
         raise TypeError("Можно сравнивать только объекты Product")
 
     def __str__(self):
-        return f"{self.name} (Количество: {self.quantity}, Цена: {self.price})"
+        return f"{self.name} (Количество: {self.quantity}, Цена: {self.get_price()})"
 
     def get_description(self) -> str:
         return f"Товар: {self.name}"
